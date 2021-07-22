b69ab9d7fdb71185f167b768104e014f0f19f825
Edward Kalu <edwardkay10@gmail.com>
2021-07-22 08:17:48 +0100

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Weather from './components/Weather';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
 
 
-ReactDOM.render(<p>Boiler Plate</p>, document.getElementById('app'));
+ReactDOM.render(<Weather />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/app.js',
+    entry: './src/App.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
     },
     module: {
         rules: [{
-            loader: 'babel-loader', 
+            loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
         }, {
